d1b2a5d6c888ccce30701f06a3972c2acb6d3f26
AnzhelikaKodlubovska <angelikagymnasticschannel@gmail.com>
2023-12-03 20:17:00 +0200

    Homework7

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clean_folder',
+    version='0.0.1',
+    description='Homework-7',
+    author='Anzhelika Kodlubovska',
+    author_email='angelikagymnasticschannel@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'clean-folder = clean_folder.clean_folder.clean:main'
+        ]
+    },
+)
